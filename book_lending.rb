3c5fc3fa8cc23171ae0a7862bcf7e6f60600dcf8
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-04-02 17:05:30 -0400

    book fin

--- book_lending.rb
+++ book_lending.rb
@@ -9,9 +9,24 @@ class Book
   end
 
   def self.current_due_date
+    @@due_date.each do |k,v|
+      if k == book
+        puts "found book"
+        return v
+      end
+    end
+    return Time.now
   end
   def self.overdue_books
-  end
+    new_book = []
+    @@due_date.each do |k,v|
+      if v <=> Time.now
+        puts "#{k} + #{v}"
+        new_book << k
+      end
+    end
+    return new_book
+    end
   def self.browse
     @@on_shelf.sample
   end
@@ -25,8 +40,10 @@ class Book
 
   @@on_shelf = []
   @@on_loan  = []
+  @@due_date = {}
 
   attr_accessor :due_date
+
 
   def initialize(title, author, isbn)
     @title = title
@@ -35,12 +52,26 @@ class Book
   end
 
   def borrow
+    if self.lent_out?
+      return false
+    end
     @@on_loan << self
+    @@on_shelf.delete(self)
+    @@due_date[self] = Time.now + (3)
+    return true
   end
 
   def return_to_library
-
-  end
+    if self.lent_out?
+         @@on_shelf << self
+         @@due_date.delete self
+         @@on_loan.delete self
+         puts "thank you"
+         return true
+       else
+         return false
+       end
+     end
 
   def lent_out?
 
